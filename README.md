4c5c23de91c82c8518cc3264658886193c3b3118
zhenxing <zhenxing@2016>
2018-06-15 10:59:46 +0800

    PropertyValuesHolders.ofKeyframe() 把同一个属性拆分

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # CircleProgressObjectAnimatorView
-自定义View绘制圆弧进度条，通过ObjectAnimator对自定义View的progress从0-65增加<br/>
+自定义View绘制圆弧进度条，通过ObjectAnimator对自定义View的progress从0-65增加<br/><br/>
+新增PropertyValuesHolders.ofKeyframe() 把同一个属性拆分<br/><br/>
 ![Image text](https://raw.githubusercontent.com/xqgdmg/CircleProgressObjectAnimatorView/master/img/Screenshot_20180613-171428.png)
 
--- app/src/main/java/dmg/xqg/com/myapplication/MainActivity.java
+++ app/src/main/java/dmg/xqg/com/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package dmg.xqg.com.myapplication;
 
+import android.animation.Keyframe;
 import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,12 +21,31 @@ public class MainActivity extends AppCompatActivity {
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 创建 ObjectAnimator 对象
-                // circleProgressObjectAnimatorView 的 progress 属性从 0 变化到 65
-                ObjectAnimator animator = ObjectAnimator.ofFloat(circleProgressObjectAnimatorView, "progress", 0, 65);
-                // 执行动画
-                animator.start();
+//                animator1(circleProgressObjectAnimatorView);
+                animator2(circleProgressObjectAnimatorView);
+
             }
         });
+    }
+
+    private void animator2(CircleProgressObjectAnimatorView circleProgressObjectAnimatorView) {
+        // 在 0% 处开始
+        Keyframe keyframe1 = Keyframe.ofFloat(0, 0);
+        // 时间经过 50% 的时候，动画完成度 100%
+        Keyframe keyframe2 = Keyframe.ofFloat(0.5f, 100);
+        // 时间见过 100% 的时候，动画完成度倒退到 80%，即反弹 20%
+        Keyframe keyframe3 = Keyframe.ofFloat(1, 80);
+        PropertyValuesHolder holder = PropertyValuesHolder.ofKeyframe("progress", keyframe1, keyframe2, keyframe3);
+
+        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(circleProgressObjectAnimatorView, holder);
+        animator.start();
+    }
+
+    private void animator1(CircleProgressObjectAnimatorView circleProgressObjectAnimatorView) {
+        // 创建 ObjectAnimator 对象
+        // circleProgressObjectAnimatorView 的 progress 属性从 0 变化到 65
+        ObjectAnimator animator = ObjectAnimator.ofFloat(circleProgressObjectAnimatorView, "progress", 0, 65);
+        // 执行动画
+        animator.start();
     }
 }
